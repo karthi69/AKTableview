6a976c5ab8eb0f8f87c95da199db2dc046800821
Karthi Anandhan <karthi.anandhan@freshworks.com>
2018-09-22 15:58:30 +0530

    Initial Commit

---
+++ AKTableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AKTableview
+//
+//  Created by Karthi Anandhan on 22/09/18.
+//  Copyright © 2018 karthi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
